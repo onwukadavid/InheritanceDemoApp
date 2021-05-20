1a76961d5bcb81da4e42f2f9f48946b588ba3e92
onwukadavid <onwukadavid21@gmail.com>
2021-05-20 11:54:57 +0100

    First commit

---
+++ InheritanceDemoApp/BindingDemo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class CParent
+{
+    public int N;
+    public virtual void Foo()
+    {
+        Console.WriteLine("in parent");
+    }
+}
+class CChild : CParent
+{
+    public new int N;
+    public new virtual void Foo()
+    {
+        Console.WriteLine("in child");
+    }
+}
+class CGrandChild : CChild
+{
+    public override void Foo()
+    {
+        Console.WriteLine("in grandchild");
+    }
+}
+class BindingDemoProgram
+{
+    static void Main(string[] args)
+    {
+        CParent p = new CGrandChild();
+        p.Foo();
+     //   p.N = 10;
+        ((CChild)p).N = 10;
+    }
+}
---
+++ InheritanceDemoApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Parent
+{
+    public int PubA;
+    private int PriA;
+    protected int ProA;
+
+    public Parent()
+    {
+    }
+    public Parent(int priA, int proA, int pubA)
+    {
+        PriA =priA;
+        ProA = proA;
+        PubA = pubA;
+    }
+}
+
+class Child : Parent
+{
+    public int PubB;
+    public Child()
+    {
+    }
+    public Child(int priA, int proA, int pubA,int pubB)
+       :base (priA,proA,pubA)//used to pass data to the parent or base class and initialize in the base class.
+    {
+       // PriA =priA;
+      //  ProA = proA;
+       // PubA = pubA;
+        PubB = pubB;
+    }
+   
+} 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Parent p;
+        Child c;
+        p = new Child();
+        //c = (Child)p;
+        c = p as Child;
+
+     }
+    }
+
